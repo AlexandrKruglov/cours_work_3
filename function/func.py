f008ecd8486a84e35d3fe06a05aea87daf9e46bf
AlexandrKruglov <krulalex84@gmail.com>
2024-02-07 00:52:40 +0300

    исправлен funk коректный вывод счетов иправлен main добавлены два теста

--- function/func.py
+++ function/func.py
@@ -1,8 +1,9 @@
 import json
-import operation_class
+import operations_cl
 from config import OPERATION_JSON
 
 path_oper = OPERATION_JSON
+
 
 def get_operations(oper):
     '''
@@ -18,19 +19,20 @@ def make_list_opration_object(oper_list):
     создаем список объектов Operation
     :return: список объектов
     '''
-
     operation_object_list = []
     for item in oper_list:
-            if item == {}:
-                continue
-            temp = operation_class.Operation(item['id'], item['date'], item['state'],
-                                             item['operationAmount']['amount'],
-                                             item['operationAmount']['currency']['name'],
-                                             item['description'], item['to'], item.get('from', ''))
-            operation_object_list.append(temp)
+        if item == {}:
+            continue
+        temp = operations_cl.Operation(item['id'], item['date'], item['state'],
+                                       item['operationAmount']['amount'],
+                                       item['operationAmount']['currency']['name'],
+                                       item['description'], item['to'], item.get('from', ''))
+        operation_object_list.append(temp)
     return operation_object_list
 
+
 operation_object_list = make_list_opration_object(get_operations(path_oper))
+
 
 def sorted_list():
     '''
@@ -47,6 +49,7 @@ def choos_executed_operation(list_op):
     '''
     executed_list_object = []
     for example in list_op:
+        print(example)
         if example.check_operation_state() == True:
             executed_list_object.append(example)
         else:
@@ -63,22 +66,28 @@ def slice_last_operation(list_op):
     new_slice_list = slice_list[::-1]
     return new_slice_list
 
+
 def add_stars_card(card):
     '''
     скрываем счет отправителя меняем цифры на звезды
     :param card:поступает счет отправителя
     '''
-    num_list = []
-    all_list = []
     card_1 = card.split(' ')  # разделяем название счета и его номер
-    card_num = card_1[1]  # выбераем номер счета
-    card_num = card_num[:6] + "******" + card_num[-4:]  # заменяем цифры на звезды
-    for i in range(0, len(card_num), 4):  # разбиваем номер по 4 знака
-        temp = card_num[i:i + 4]
-        num_list.append(temp)
-    all_list.append(card_1[0])  # соединяем обратно название счета с номером
-    all_list.extend(num_list)
-    new_card = " ".join(all_list)
+    if card_1[0] == 'Счет':  # проверяем счет или карта если счет вызываем фун. скрыть счет
+        return add_star_to_chek(card_1[1])
+    else:
+        new_card_temp = []
+        num_list = []
+        for item in card_1:
+            if item.isdigit() is False:  # проверяем название карты или номер
+                new_card_temp.append(item)
+            else:
+                card_num = item[:6] + "******" + item[-4:]  # заменяем цифры на звезды
+                for i in range(0, len(card_num), 4):  # разбиваем номер по 4 знака
+                    temp = card_num[i:i + 4]
+                    num_list.append(temp)
+    new_card_temp.extend(num_list)  # соединяем обратно название счета с номером
+    new_card = " ".join(new_card_temp)
     return new_card
 
 
@@ -90,10 +99,13 @@ def add_star_to_chek(chek):
     return new_chek
 
 
-def output_operation(list_operation):
+def output_operation():
     '''
     вывод операции в нужном формате
     '''
+    data = sorted_list()  # список отсортираванный по времени
+    data_executed = choos_executed_operation(data)  # список только выполненых операций
+    list_operation = slice_last_operation(data_executed)  # срез последних 5 операций
     for item in list_operation:
         print(f"{item.return_date()} {item.description}")
         if item.operation_from == "":
--- test/test_func.py
+++ test/test_func.py
@@ -1,52 +1,37 @@
-import pytest
 from function import func
-from function import operation_class
-import main
+from function import operations_cl
 
-
-class For_test:
-    def __init__(self, first, second):
-        self.first = first
-        self.second =second
-    def check_operation_state(self):
-        if self.first == 1:
-            return True
-        else:
-            return False
-
-test_class = For_test(1,2)
-test_class1 = For_test(3,4)
-
-
-def test_main():
-    assert main.data != func.sorted_list()
 
 def test_slice_last_operation():
     assert func.slice_last_operation([1, 2, 3, 4, 5, 6, 7, 8]) == [8, 7, 6, 5, 4]
 
-def test_make_list_opration_object():
-    json = [{}]
-    json_1 = [{
-    "id": 957763565,
-    "state": "EXECUTED",
-    "date": "2019-01-05T00:52:30.108534",
-    "operationAmount": {
-      "amount": "87941.37",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод со счета на счет",
-    "from": "Счет 46363668439560358409",
-    "to": "Счет 18889008294666828266"
-  }]
-    assert func.make_list_opration_object(json) == []
-    assert func.make_list_opration_object(json_1) != [operation_class.Operation(957763565, "2019-01-05T00:52:30.108534",
-                                                                                  "EXECUTED", "87941.37", "руб.", "Перевод со счета на счет",
-                                                                                "Счет 18889008294666828266", "Счет 46363668439560358409")]
 
-def test_choos_executed_operation():
-     assert func.choos_executed_operation([test_class, test_class1]) == [test_class]
-     assert func.choos_executed_operation([test_class1]) == []
-     assert func.choos_executed_operation([]) == []
+def test_add_stars_card():
+    assert func.add_stars_card("carta 12345678900987654321") == "carta 1234 56** **** 4321"
+    assert func.add_stars_card("Счет 12345678900987654321") == "Счет **4321"
+    assert func.add_stars_card("card card") == "card card"
+
+
+def test_add_star_to_chek():
+    assert  func.add_star_to_chek("счет 555555555") == "Счет **5555"
+
+
+def test_make_list_opration_object():
+    json = [{
+             "id": 957763565,
+             "state": "EXECUTED",
+             "date": "2019-01-05T00:52:30.108534",
+             "operationAmount": {
+                 "amount": "87941.37",
+                 "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                    }
+                 },
+             "description": "Перевод со счета на счет",
+             "from": "Счет 46363668439560358409",
+             "to": "Счет 18889008294666828266"
+            }]
+    result = func.make_list_opration_object(json)
+    operation = result[0]
+    assert isinstance(operation, operations_cl.Operation)
